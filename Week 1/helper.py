b6689e5b0fd1d6073b260464b8742b863b0a5f07
Ashwin Balaraman <abalaraman1@uchicago.edu>
2024-10-22 21:56:53 -0500

    Initialized a repository for Maroon Capital weekly blotto problems

---
+++ Week 1/helper.py
@@ -0,0 +1,4 @@
+import numpy
+
+def check_each():
+    
---
+++ Week 1/simulation.py
@@ -0,0 +1,8 @@
+import helper
+
+def simulate():
+    for ___ in range(10):
+        #Check for every opponent's setup with mine to find worst case
+        pass
+
+
